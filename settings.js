ff9c39955bd414a76b92442639aec81cc8fa415e
Bicomaru120 <bicomarudono@gmail.com>
2024-08-02 21:27:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "2349072642884"
+global.ownername = "🐛 EmmyGold"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '23490722642884'  //creator number
+global.ownername = '🐛 EmmyGold' //owner name
+global.botname = 'EmmyGold ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Tech-God\n\nContact: +2349072642884'
 
 //console view/theme
 global.themeemoji = '🪀'
